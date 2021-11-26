d96b128f90ca3a3eead67be5fe3b270524ac270b
jermin88 <95072494+jermin88@users.noreply.github.com>
2021-11-26 20:29:23 +0800

    Update insert_username.php

--- insert_username.php
+++ insert_username.php
@@ -1,19 +1,20 @@
 <html>
 <body>
 <?php
-$con = mysqli_connect("localhost", "root","","tp_amc"); //connect to database
+$con = mysqli_connect("localhost", "root","","tpamc"); //connect to database
 if (!$con) {
     die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail  
 }
 
-$query= $con->prepare("INSERT INTO `user` (`NAME`,`USERNAME`,`PASSWORD`, `ROLE`) VALUES
-(?,?,?,?)");
+$query= $con->prepare("INSERT INTO `user` (`NAME`,`USERNAME`,`PASSWORD`, `ROLE`, `ADDRESS`) VALUES
+(?,?,?,?,?)");
 
-$name = 'Eileen';
-$pwd = 'eileen1';
+$name = 'Aaron';
+$pwd = 'Aaron88';
 $role = 'ADMIN';
+$address = 'Blk 333 Bedok';
 
-$query->bind_param('ssss', $name, $name, $pwd, $role); //bind the parameters
+$query->bind_param('sssss', $name, $name, $pwd, $role, $address); //bind the parameters
 
 if ($query->execute()){ //execute query
     echo "Query executed.";
