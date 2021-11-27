a0035058d6d6c24ebd5a56a7cf45cad749dbf19e
koyorii <53043482+koyorii@users.noreply.github.com>
2021-11-27 21:57:03 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,17 +1,13 @@
+<?php
+session_start()
+?>
 <html>
 <head>
 <title>Safe Lights</title>
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <!--nav bar-->
-    <nav class="navMenu">
-      <a href="#">Home</a>
-      <a href="#">Products</a>
-      <a href="#">About</a>
-      <a href="loginform.php">Login</a>
-      <div class="dot"></div>
-    </nav>
+<?php include "nav-bar.php"?>
       
 </body>
 </html>
