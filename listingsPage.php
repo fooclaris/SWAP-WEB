97b9b89a8a6de0e339c175d04f3e67d6f4867091
iwillhackyou <95073886+iamhackerman@users.noreply.github.com>
2021-11-28 21:38:07 +0800

    Create listingsPage.php
    i am gay

---
+++ listingsPage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<p style ="text-align:center;"><font size="+5">Products</font></p>
+<p>
+<a href ="http://localhost/swap/listingDetails.php">
+<img src="flashlight.jpg" alt="flashlight" width="150" height="150" />
+</a> 
+</p>
+</body>
+</html>
